79181420eb46dd0a16a962ff70717b2b1bc8912a
igarcia <ignacio105garcia@gmail.com>
2024-12-19 14:32:27 -0600

    refactor

--- src/photos/photos.controller.ts
+++ src/photos/photos.controller.ts
@@ -1,60 +1,87 @@
 import {
-  Controller,
-  Post,
-  UseInterceptors,
-  UploadedFile,
-  BadRequestException,
-  Body,
+    Controller,
+    Post,
+    UseInterceptors,
+    UploadedFile,
+    BadRequestException,
+    Body,
+    Delete,
+    Param,
+    HttpStatus,
+    HttpCode,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
 import { PhotosService } from './photos.service';
 
-
 @Controller('photos')
 export class PhotosController {
-  constructor(private readonly photosService: PhotosService) {}
+    constructor(private readonly photosService: PhotosService) { }
 
-  @Post('upload')
-  @UseInterceptors(
-    FileInterceptor('file', {
-      storage: diskStorage({
-        destination: './uploads/photos',
-        filename: (req, file, callback) => {
-          const uniqueSuffix =
-            Date.now() + '-' + Math.round(Math.random() * 1e9);
-          callback(null, `${uniqueSuffix}${extname(file.originalname)}`);
+    /*@Post('upload')
+    @UseInterceptors(
+      FileInterceptor('file', {
+        storage: diskStorage({
+          destination: './uploads/photos',
+          filename: (req, file, callback) => {
+            const uniqueSuffix =
+              Date.now() + '-' + Math.round(Math.random() * 1e9);
+            callback(null, `${uniqueSuffix}${extname(file.originalname)}`);
+          },
+        }),
+        fileFilter: (req, file, callback) => {
+          const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];//preguntar por el tipo de imagen
+          if (!allowedMimeTypes.includes(file.mimetype)) {
+            return callback(
+              new BadRequestException('Tipo de archivo no permitido'),
+              false,
+            );
+          }
+          callback(null, true);
+        },
+        limits: {
+          fileSize: 5 * 1024 * 1024,
         },
       }),
-      fileFilter: (req, file, callback) => {
-        const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
-        if (!allowedMimeTypes.includes(file.mimetype)) {
-          return callback(
-            new BadRequestException('Tipo de archivo no permitido'),
-            false,
-          );
+    )
+    async uploadPhoto(
+      @UploadedFile() file: Express.Multer.File,
+      @Body('profileId') profileId: string,
+    ) {
+      if (!file) {
+        throw new BadRequestException('No se proporcionó ningún archivo');
+      }
+  
+      const photo = await this.photosService.createPhoto({
+          url: `uploads/photos/${file.filename}`,
+          profileId,
+        });
+  
+      return photo;
+    }*/
+
+    @Post('upload')
+    @UseInterceptors(
+        FileInterceptor('file', PhotosService.prototype.getMulterOptions()),
+    )
+    async uploadPhoto(
+        @UploadedFile() file: Express.Multer.File,
+        @Body('profileId') profileId: string,
+    ) {
+        if (!file) {
+            throw new BadRequestException('NO_SE_PROPORCIONO_NINGUN_ARCHIVO');
         }
-        callback(null, true);
-      },
-      limits: {
-        fileSize: 5 * 1024 * 1024,
-      },
-    }),
-  )
-  async uploadPhoto(
-    @UploadedFile() file: Express.Multer.File,
-    @Body('profileId') profileId: string,
-  ) {
-    if (!file) {
-      throw new BadRequestException('No se proporcionó ningún archivo');
+
+        return this.photosService.createPhoto({
+            url: `uploads/photos/${file.filename}`,
+            profileId,
+        });
     }
 
-    const photo = await this.photosService.createPhoto({
-        url: `uploads/photos/${file.filename}`,
-        profileId,
-      });
-
-    return photo;
-  }
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async deletePhoto(@Param('id') photoId: string) {
+        await this.photosService.deletePhoto(photoId);
+    }
 }
--- src/photos/photos.service.ts
+++ src/photos/photos.service.ts
@@ -1,21 +1,91 @@
-import { Injectable } from '@nestjs/common';
+import {
+    BadRequestException,
+    Injectable,
+    InternalServerErrorException,
+    NotFoundException,
+} from '@nestjs/common';
 import { CreatePhotoDto } from './dto/create-photo.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class PhotosService {
-  constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) { }
 
-  async createPhoto(data: CreatePhotoDto) {
-    return this.prisma.photo.create({
-      data: {
-        url: data.url,
-        profile: {
-          connect: {
-            id: data.profileId
-          }
+    getMulterOptions() {
+        return {
+            storage: diskStorage({
+                destination: './uploads/photos',
+                filename: (req, file, callback) => {
+                    const uniqueSuffix =
+                        Date.now() + '-' + Math.round(Math.random() * 1e9);
+                    callback(null, `${uniqueSuffix}${extname(file.originalname)}`);
+                },
+            }),
+            fileFilter: (req, file, callback) => {
+                const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+                if (!allowedMimeTypes.includes(file.mimetype)) {
+                    return callback(
+                        new BadRequestException('Tipo de archivo no permitido'),
+                        false,
+                    );
+                }
+                callback(null, true);
+            },
+            limits: {
+                fileSize: 5 * 1024 * 1024, // 5MB
+            },
+        };
+    }
+
+    async createPhoto(data: CreatePhotoDto) {
+        return this.prisma.photo.create({
+            data: {
+                url: data.url,
+                profile: {
+                    connect: {
+                        id: data.profileId,
+                    },
+                },
+            },
+        });
+    }
+
+    /*async deletePhoto(id: string) {
+      const photo = await this.prisma.photo.findUnique({ where: { id: id }});
+      if(!photo) throw new NotFoundException('NOT_FOUND');
+      
+      return this.prisma.photo.delete({ where: {id: id}});
+    }*/
+
+    async deletePhoto(photoId: string) {
+        const photo = await this.prisma.photo.findUnique({
+            where: { id: photoId },
+        });
+
+        try {
+            const archivoGuardado = photo.url;
+
+            console.log(photoId);
+            console.log(archivoGuardado);
+
+            /*if (!photo || !fs.existsSync(archivoGuardado))
+                throw new NotFoundException('NOT_FOUND');*/
+
+            if (fs.existsSync(archivoGuardado)) {
+                fs.unlinkSync(archivoGuardado);
+            } else {
+                console.warn(`no encontrado --> ${archivoGuardado}`);
+            }
+        } catch (error) {
+           // console.error(`error --> ${archivoGuardado}`, error);
+            throw new InternalServerErrorException('ERROR_DELETING_FILE');
+            //throw new HttpException('ERROR', 403);
         }
-      }
-    });
-  }
+
+        await this.prisma.photo.delete({ where: { id: photoId } });
+    }
 }
